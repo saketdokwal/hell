c247138b2323f1ff51e4e04c79e53a5db869e5a0
saketdokwal <suyashs097@gmail.com>
2024-06-05 19:11:43 +0530

    hey new commit

--- hell.js
+++ hell.js
@@ -1,6 +1,20 @@
-const a = ["ram", "Sham", "Dev", "Prakash"];
-console.log(a);
-a.sort();
-console.log(a);
-a.reverse();
-console.log(a);
+// const a = ["ram", "Sham", "Dev", "Prakash"];
+// console.log(a);
+// a.sort();
+// console.log(a);
+// a.reverse();
+// console.log(a);
+// concat function  in js
+
+// var a = ["saket", "Aman", "sanjay"];
+// var b = ["Hemant", "Nmait"];
+// var c = ["priya", "khushi"];
+// var d = a.concat(b, c);
+// console.log(d);
+
+// for each
+// it executes a function once for each array element.
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+arr.forEach(function (element) {
+  console.log(element);
+});
